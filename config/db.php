526d25547a84765c6167f5381d37a877ec98177b
Madelein Pretorius <50348491+maddiepret@users.noreply.github.com>
2020-06-19 14:03:52 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 #CONNECT TO DATABSE
 // takes 4 params, localhost, user, password and db name
 
-$conn = mysqli_connect('localhost', 'root', 'Softlips22', 'phpblog');
+$conn = mysqli_connect('localhost', 'root', '*******', 'phpblog');
 // $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_USER);
 
 
